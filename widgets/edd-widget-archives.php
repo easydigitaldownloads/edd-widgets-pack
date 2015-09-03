055cc56b7df2a60dda082102dc0feda20a870711
John Parris <public@johnparris.com>
2015-09-03 16:59:49 -0400

    Second pass at not breaking queries.
    Removes the pre_get_posts action in the archives widget. Appends the ?post_type=download to the archive links using the month_link filter.

--- widgets/edd-widget-archives.php
+++ widgets/edd-widget-archives.php
@@ -1,5 +1,5 @@
 <?php
-/** 
+/**
  * EDD Archives Widget
  *
  * @package      EDD Widgets Pack
@@ -14,7 +14,7 @@
  * EDD Archives Widget Class
  *
  * EDD Downloads archive.
- *  
+ *
  * @access   private
  * @return   void
  * @since    1.0
@@ -38,13 +38,9 @@ if ( ! class_exists( 'EDD_Archives' ) ) {
             add_action( 'update_option_start_of_week', array( &$this, 'delete_cache' ) );
             add_action( 'update_option_gmt_offset', array( &$this, 'delete_cache' ) );
 
-            // enable date based archives for downloads
-            add_action( 'pre_get_posts', array( &$this, 'pre_get_posts_filter' ) );
-            
             // contruct widget
             parent::__construct( false, __( 'EDD Archives', 'edd-widgets-pack' ), array( 'description' => sprintf( __( 'A monthly archive of your site\'s %s.', 'edd-widgets-pack' ), edd_get_label_plural( true ) ) ) );
         }
-
 
         /**
          * Widget
@@ -60,7 +56,7 @@ if ( ! class_exists( 'EDD_Archives' ) ) {
 
                 // get the title and apply filters
                 $title = apply_filters( 'widget_title', $instance['title'] ? $instance['title'] : '' );
-                
+
                 // get show count boolean
                 $show_count = isset( $instance['show_count'] ) && $instance['show_count'] === 1 ? 1 : 0;
 
@@ -77,12 +73,16 @@ if ( ! class_exists( 'EDD_Archives' ) ) {
 
                 // add download post type to archives
                 add_filter( 'getarchives_where' , array( &$this, 'getarchives_where_filter' ) , 10 , 2 );
-                
+
+                add_filter( 'month_link', array( $this, 'month_link' ), 10, 3 );
+
                 // output the archives
                 $out .= wp_get_archives( array( 'echo' => 0, 'show_post_count' => $show_count ) );
-                
+
                 // remove filter
                 remove_filter( 'getarchives_where' , array( &$this, 'getarchives_where_filter' ), 10, 2 );
+
+                remove_filter( 'month_link', array( $this, 'month_link' ), 10, 3 );
 
                 // finish the list
                 $out .= "</ul>\n";
@@ -91,7 +91,7 @@ if ( ! class_exists( 'EDD_Archives' ) ) {
                 $cache = $args['before_widget'] . $out . $args['after_widget'];
 
                 // store the result on a temporal transient
-                set_transient( 'edd_widgets_archives', $cache );
+                set_transient( 'edd_widgets_archives', $cache, DAY_IN_SECONDS );
 
             }
 
@@ -106,10 +106,22 @@ if ( ! class_exists( 'EDD_Archives' ) ) {
          * @return   string
          * @since    1.0
         */
-        
+
         function getarchives_where_filter( $where , $r )
         {
             return str_replace( "post_type = 'post'" , "post_type = 'download'" , $where );
+        }
+
+
+        /**
+         * Filters the month link so the links go to the
+         * date archives for the download post type.
+         *
+         * @since 1.3
+         * @return string
+         */
+        function month_link( $monthlink, $year, $month ) {
+            return $monthlink . '?post_type="download"';
         }
 
 
@@ -121,12 +133,12 @@ if ( ! class_exists( 'EDD_Archives' ) ) {
         */
 
         function update( $new_instance, $old_instance )
-        {     
+        {
             $instance = $old_instance;
 
             // sanitize title
             $instance['title'] = strip_tags( $new_instance['title'] );
-            
+
             // sanitize show price
             $instance['show_count'] = strip_tags( $new_instance['show_count'] );
             $instance['show_count'] = $instance['show_count'] === '1' ? 1 : 0;
@@ -143,7 +155,7 @@ if ( ! class_exists( 'EDD_Archives' ) ) {
          *
          * @return   void
          * @since    1.0
-        */    
+        */
 
         function delete_cache()
         {
@@ -169,42 +181,9 @@ if ( ! class_exists( 'EDD_Archives' ) ) {
                 </p>
                 <p>
                     <input id="<?php echo $this->get_field_id( 'show_count' ); ?>" name="<?php echo $this->get_field_name( 'show_count' ); ?>" type="checkbox" value="1" <?php checked( '1', $show_count ); ?>/>
-                    <label for="<?php echo $this->get_field_id( 'show_count' ); ?>"><?php printf( __('Show %s counts?', 'edd-widgets-pack' ), edd_get_label_plural( true ) ); ?></label> 
+                    <label for="<?php echo $this->get_field_id( 'show_count' ); ?>"><?php printf( __('Show %s counts?', 'edd-widgets-pack' ), edd_get_label_plural( true ) ); ?></label>
                 </p>
             <?php
-        }
-
-
-        /**
-         * Rewrite Rules Array
-         *
-         * @author   Copyright 2012 Jennifer M. Dodd <jmdodd@gmail.com>
-         * @license  Released under the GPLv2 ( or later ).
-         * @return   void
-         * @since    1.0
-        */
-
-        function pre_get_posts_filter( $query ) {
-
-            if ( ! is_active_widget( false, false, $this->id_base, true ) )
-            return;
-
-            if ( ! is_preview() && ! is_admin() && ! is_singular() && ! is_404() && ! is_home() ) {
-                if ( ! $query->is_feed ) {
-                    $my_post_type = get_query_var( 'post_type' );
-                    if ( empty( $my_post_type ) ) {
-                        $args = array( 
-                            'public' => true , 
-                            '_builtin' => false
-                         );
-                        $output = 'names';
-                        $operator = 'and';
-
-                        $post_types = array_merge( array( 'download' ), array( 'post' ) );
-                        $query->set( 'post_type', $post_types );
-                    }
-                }
-            } 
         }
 
     }
@@ -213,7 +192,7 @@ if ( ! class_exists( 'EDD_Archives' ) ) {
 
 /**
  * Register Archives Widget
- *  
+ *
  * @access   private
  * @return   void
  * @since    1.0
