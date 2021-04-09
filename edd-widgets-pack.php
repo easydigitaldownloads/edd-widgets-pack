20a448610fee708c0573160e11399131cc53b3fc
Robin Cornett <hello@robincornett.com>
2021-04-09 09:27:00 -0400

    Update plugin author, URI, item ID
    #15

--- edd-widgets-pack.php
+++ edd-widgets-pack.php
@@ -1,14 +1,14 @@
 <?php
-/*
-Plugin Name: Easy Digital Downloads - Widgets Pack
-Plugin URL: http://easydigitaldownloads.com/extensions/widgets-pack
-Description: A pack of widgets for Easy Digital Downloads.
-Version: 1.2.6
-Author: Easy Digital Downloads
-Author URI: http://easydigitaldownloads.com
-Contributors: sksmatt
-Text Domain: edd-widgets-pack
-*/
+/**
+ * Plugin Name: Easy Digital Downloads - Widgets Pack
+ * Plugin URL: https://easydigitaldownloads.com/downloads/widgets-bundle/
+ * Description: A pack of widgets for Easy Digital Downloads.
+ * Version: 1.2.6
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Contributors: sksmatt
+ * Text Domain: edd-widgets-pack
+ */
 
 /**
  * Initalization
@@ -22,28 +22,29 @@ Text Domain: edd-widgets-pack
 */
 
 if ( ! function_exists( 'edd_widgets_pack_init' ) ) {
-    function edd_widgets_pack_init() {
+	function edd_widgets_pack_init() {
 
-        if( ! function_exists( 'edd_price' ) )
-            return;
+		if ( ! function_exists( 'edd_price' ) ) {
+			return;
+		}
 
-        // load internationalization
-        load_plugin_textdomain( 'edd-widgets-pack', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		// load internationalization
+		load_plugin_textdomain( 'edd-widgets-pack', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-        // Handle licensing
-        if( class_exists( 'EDD_License' ) ) {
-            $license = new EDD_License( __FILE__, 'Widgets Pack', '1.2.6', 'EDD Team' );
-        }
+		// Handle licensing
+		if ( class_exists( 'EDD_License' ) ) {
+			$license = new EDD_License( __FILE__, 'Widgets Pack', '1.2.6', 'Sandhills Development, LLC', null, null, 1514 );
+		}
 
-        // register widgets
-        require_once( plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-top-sellers.php' );
-        require_once( plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-most-commented.php' );
-        require_once( plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-most-recent.php' );
-        require_once( plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-related-downloads.php' );
-        require_once( plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-featured-download.php' );
-        require_once( plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-random-download.php' );
-        require_once( plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-archives.php' );
-        require_once( plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-downloads-calendar.php' );
-    }
+		// register widgets
+		require_once plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-top-sellers.php';
+		require_once plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-most-commented.php';
+		require_once plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-most-recent.php';
+		require_once plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-related-downloads.php';
+		require_once plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-featured-download.php';
+		require_once plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-random-download.php';
+		require_once plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-archives.php';
+		require_once plugin_dir_path( __FILE__ ) . 'widgets/edd-widget-downloads-calendar.php';
+	}
 }
 add_action( 'plugins_loaded', 'edd_widgets_pack_init' );
